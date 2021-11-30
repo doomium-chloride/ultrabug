661441ef50d123c375d6867c8c64f9022326878f
doomium-chloride <47913762+doomium-chloride@users.noreply.github.com>
2021-11-30 18:31:42 +1300

    Using firewalls
    Also add transparency to skip cutscene option

--- Scripts/Lshape.gd
+++ Scripts/Lshape.gd
@@ -1,8 +1,8 @@
 extends "res://Scripts/BaseMap.gd"
 
 func _ready():
-	walls_needed = 40
-	chips_needed = 1
+	walls_needed = 50
+	chips_needed = 2
 	next_scene = "res://Screens/Win.tscn"
 	update_ui()
 	Global.emit_signal("set_last_level", "res://Maps/Lshape.tscn")
